c3fee21b4b599854ab85e2b9741c5a19c4686c6f
Sayyad Yasmin <syedyasmin2005@gmail.com>
2026-04-15 21:30:00 +0530

    Refactor DecisionMakingStatements.java
    Removed comment about ternary operator and cleaned up code.

--- Day1/DecisionMakingStatements.java
+++ Day1/DecisionMakingStatements.java
@@ -58,8 +58,6 @@ class DecisionMakingStatements
                     System.out.println("enter valid day!");
                     break;
             }
-            //ternary operator
             System.out.println(age>=18? "eligible":"not eligible");
-
     }
 }
