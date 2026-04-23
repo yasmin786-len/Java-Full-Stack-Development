db2545ea7aa6cab6f82eb6a52fd94ebe71664939
Sayyad Yasmin <syedyasmin2005@gmail.com>
2026-04-23 18:13:20 +0530

    Remove empty line in pattern10.java

--- DSA/Loops/pattern10.java
+++ DSA/Loops/pattern10.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class pattern10 {
     public static void main(String[] args) {
-        
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
     for(int i=1;i<=2*n-1;i++){
