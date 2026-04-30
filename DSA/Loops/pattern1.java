7676a79202a30ff0d524635426772145e597f9bd
Sayyad Yasmin <syedyasmin2005@gmail.com>
2026-04-30 22:04:50 +0530

    Refactor pattern1.java for better readability
    Fix formatting issues and improve readability in pattern1.java.

--- DSA/Loops/pattern1.java
+++ DSA/Loops/pattern1.java
@@ -5,7 +5,6 @@ public class pattern1 {
         
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
-
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
             System.out.print("*"+" ");
